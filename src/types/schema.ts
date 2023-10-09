206c4f73eac3c1c74ac9b4a6346b6df8bbbbaeb5
jhordy <jhooordy@gmail.com>
2023-10-09 20:01:21 -0300

    chore: update schema

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -3,20 +3,20 @@ import { z } from 'zod';
 export const TransformedObjectRating = z.record(z.number());
 
 export const RatingSchema = z.object({
-  placeName: z.string().nullable().optional(),
+  placeName: z.string(),
   rating: TransformedObjectRating,
   averageRating: z.number(),
   totalReviews: z.number(),
 });
 
 const ReviewsSchema = z.object({
-  userName: z.string().optional(),
-  userAvatarUrl: z.string().nullable().optional(),
-  rating: z.number().optional(),
-  date: z.string().nullable(),
+  userName: z.string(),
+  userAvatarUrl: z.string().nullable(),
+  rating: z.number(),
+  date: z.string(),
   comment: z.string().optional(),
   images: z.array(z.string()),
-  reviewId: z.string().nullable(),
+  reviewId: z.string(),
 });
 
 export const ReviewsSchemaResponse = z.object({
